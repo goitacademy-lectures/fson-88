2b2d485c6c303af2ff69a982adf7fb4332968c7b
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-05-13 02:19:12 +0300

    add module 06 lesson 02

--- js/form.js
+++ js/form.js
@@ -1,3 +1,21 @@
-document.querySelector('[name="booking_form"]').addEventListener('submit', event => {
+const formEl = document.querySelector('[name="booking_form"]');
+const phoneEl = document.querySelector('.booking-form-input[type="tel"]');
+const formIconEl = document.querySelector('[data-form-icon]');
+const formFieldEl = document.querySelector('[data-form-field]');
+
+Inputmask('+38 (0**) 999-99-99', {
+  placeholder: '*',
+  showMaskOnHover: false,
+  oncomplete: () => {
+    formIconEl.classList.add('valid-phone');
+    formFieldEl.classList.add('valid-field');
+  },
+  oncleared: () => {
+    formIconEl.classList.remove('valid-phone');
+    formFieldEl.classList.remove('valid-field');
+  },
+}).mask(phoneEl);
+
+formEl.addEventListener('submit', event => {
   event.preventDefault();
 });
